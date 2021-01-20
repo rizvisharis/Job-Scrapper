6dc7f93a5e69fb67d030f254663c18c22816688c
Rizvi <rizvisharis@gmail.com>
2021-01-20 19:09:36 +0530

    initilize the temporary database

--- flask_try.py
+++ flask_try.py
@@ -3,6 +3,7 @@ from scrapper import get_jobs
 
 app = Flask("SuperScarpper")
 
+db  ={}
 @app.route("/")
 def home():
     return render_template("form.html")
@@ -16,11 +17,15 @@ def report():
     word = request.args.get('word', '')
     if word:
         word = word.lower()
-        jobs = get_jobs(word)
-        print(jobs)
+        fromDb = db.get(word)
+        if fromDb:
+            jobs = fromDb
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
 
-    return render_template("report.html", searching_by = word)
+    return render_template("report.html", searching_by = word, resultsNumber = len(jobs))
 
 app.run()
